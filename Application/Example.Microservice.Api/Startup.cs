983b5b3a52bcae28b120ea64f4bf3a5627dfc903
Thomas Kitzmiller <kitzy78@yahoo.com>
2019-09-06 10:54:03 -0400

    Configured API project

--- Application/Example.Microservice.Api/Startup.cs
+++ Application/Example.Microservice.Api/Startup.cs
@@ -1,34 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Reflection;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Safelite.Common.Extensions.HealthChecks;
 
 namespace Example.Microservice.Api
 {
   public class Startup
   {
+    private readonly IConfiguration configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+      this.configuration = configuration;
+    }
+
     // This method gets called by the runtime. Use this method to add services to the container.
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+      services.AddMvc()
+        .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_2);
+
+      services.AddHealthChecks()
+        .AddMemoryHealthCheck();
+
+      services.SetHealthCheckPublisherOptions(configuration);
+
+      services.AddMvcCore();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IHostingEnvironment env)
     {
+      app.UseHealthChecks(configuration);
+
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
       }
 
-      app.Run(async (context) =>
-      {
-        await context.Response.WriteAsync("Hello World!");
-      });
+      app.UseMvc();
     }
   }
 }
